d8592d8ae7170cd17b48eb824f9d1931f2ecf8d0
zion928 <zion010928@gmail.com>
2022-11-10 21:03:38 +0900

    first commit

---
+++ 2525.py
@@ -0,0 +1,10 @@
+hour, min = map(int, input().split())
+
+add = int(input())
+if 0 <= hour <= 23 and 0 <= min <= 59:
+    if min + add >= 60:
+        hour = int(hour + (min+add)/60) %24
+        min = (min + add) % 60
+    else:
+        min = min+add
+print(f"{hour} {min}")
---
+++ 2884.py
@@ -0,0 +1,13 @@
+hour, min = map(int, input().split())
+if 0 <= hour <= 23 and 0 <= min <= 59:
+    if min < 45:
+        if hour > 0:
+            hour = hour - 1
+            min = min + 15
+        else:
+            hour = 23 + hour
+            min = min + 15
+    else:
+        min = min - 45
+
+print(f"{hour} {min}")
